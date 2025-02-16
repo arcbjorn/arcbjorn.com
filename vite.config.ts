4092873517c0d1174f43cc03c4bdefb9e6ac8f19
arcbjorn <arky.happ@gmail.com>
2025-02-16 01:08:34 -0300

    chore: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [solidPlugin(), tailwindcss()],
@@ -9,5 +10,15 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@state': path.resolve(__dirname, './src/state'),
+      '@i18n': path.resolve(__dirname, './src/i18n'),
+    },
   },
 });
