4345de1e0e3cfadeb188f36233aba156626fe59c
arcbjorn <arky.happ@gmail.com>
2025-02-16 15:28:28 -0300

    ref: add i18n types

---
+++ src/i18n/types.ts
@@ -0,0 +1,44 @@
+export enum Ei18nToken {
+  NAME = 'name',
+  LOCATION = 'header.location',
+
+  PAGE_NOT_FOUND = 'notFound.title',
+  GO_BACK = 'notFound.backButtonTitle',
+
+  ABOUT = 'header.about',
+  EXTRA = 'header.extra',
+
+  FILE = 'home.file',
+  GREETING = 'home.greeting',
+  POSITION = 'home.position',
+  FORMERLY = 'home.formerly',
+  INTERESTS = 'home.interests',
+  CV = 'home.cv',
+
+  PLATFORMS_TITLE = 'extra.platforms.title',
+
+  BLOG_TITLE = 'extra.platforms.names.blog',
+  MY_TRAVEL_MAP_TITLE = 'extra.platforms.names.myTravelMap',
+
+  BOOKS_TITLE = 'extra.books.title',
+
+  SOFTWARE = 'extra.books.categories.software',
+  PSYCHOLOGY = 'extra.books.categories.psychology',
+  FANTASY = 'extra.books.categories.fantasy',
+
+  LANGUAGES_TITLE = 'extra.languages.title',
+
+  LANG_ENGLISH_NAME = 'extra.languages.english',
+  LANG_RUSSIAN_NAME = 'extra.languages.russian',
+  LANG_GERMAN_NAME = 'extra.languages.german',
+  LANG_SPANISH_NAME = 'extra.languages.spanish',
+  LANG_PORTUGUESE_NAME = 'extra.languages.portuguese',
+  LANG_JAPANESE_NAME = 'extra.languages.japanese',
+  LANG_SWEDISH_NAME = 'extra.languages.swedish',
+
+  ALL_SOCIALS = 'extra.platforms.allSocials',
+
+  LANG_LEVEL_NATIVE = 'extra.languages.levels.native',
+  LANG_LEVEL_FLUENT = 'extra.languages.levels.fluent',
+  LANG_LEVEL_BASIC = 'extra.languages.levels.basic',
+}
