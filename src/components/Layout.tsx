703c0e0f668d7cfc7a7b711def8b7afa44a5d59b
arcbjorn <arky.happ@gmail.com>
2025-02-16 19:12:44 -0300

    feat(components): add Layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import { Component, JSX } from 'solid-js';
+import Header from '@/components/Header';
+
+interface LayoutProps {
+  children: JSX.Element;
+}
+
+export const Layout: Component<LayoutProps> = props => {
+  return (
+    <main class="container mx-auto h-full">
+      <header class="h-1/10">
+        <Header />
+      </header>
+      <section class="h-5/6">{props.children}</section>
+    </main>
+  );
+};
+
+export default Layout;
