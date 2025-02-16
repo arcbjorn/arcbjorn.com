785f79e75284c66f6907d2affc1d89f0607f6ba9
arcbjorn <arky.happ@gmail.com>
2025-02-16 20:11:25 -0300

    feat: add types

---
+++ src/types.ts
@@ -0,0 +1,85 @@
+import { IconName } from '@fortawesome/fontawesome-svg-core';
+import { Ei18nToken } from '@i18n/types';
+
+export enum EDocumentDescription {
+  INDEX_PAGE = '',
+  EXTRA = 'extra',
+  COPY_LINKS = 'copy_links',
+  NOT_FOUND_PAGE = 'Not Found',
+}
+
+export type TLanguage = {
+  name: string;
+  level: number;
+};
+
+export enum EBookCategory {
+  SOFTWARE = 'Software',
+  PSYCHOLOGY = 'Psychology',
+  FANTASY = 'Fantasy',
+}
+
+export type TBook = {
+  author: string;
+  title: string;
+  category: EBookCategory;
+  href: string;
+};
+
+export enum EIconLibrary {
+  FONT_AWESOME = 'font-awesome',
+  MATERIAL = 'material',
+  CUSTOM = 'custom',
+}
+
+export type TLink<TLinkPartial = Record<string, string>> = TLinkPartial & {
+  href: ELinkUrl;
+  iconName: IconName | string;
+  iconPrefix: EIconLibrary;
+  iconTitle: string;
+};
+
+export type IHomePageLinkPartial = {
+  title: ELinkTitle | Ei18nToken;
+};
+
+export interface IPlatformLinkPartial {
+  title: ELinkTitle | Ei18nToken;
+}
+
+export enum ELinkTitle {
+  LINKEDIN = 'LinkedIn',
+  GITHUB = 'Github',
+  EMAIL = 'EMail',
+
+  BLOG = 'Blog',
+  LEETCODE = 'LeetCode',
+  MY_TRAVEL_MAP = 'My travel map',
+
+  TWITTER = 'Twitter',
+  REDDIT = 'Reddit',
+  INSTAGRAM = 'Instagram',
+  SPOTIFY = 'Spotify',
+  GOOD_READS = 'GoodReads',
+}
+
+export enum ELinkUrl {
+  LINKEDIN = 'https://www.linkedin.com/in/oleg-luganskiy/',
+  GITHUB = 'https://www.github.com/arcbjorn',
+  MAIL = 'mailto:oleg.luganskiy@gmail.com?subject=Employment%20Offer&body=Hi%20Oleg',
+  CV = 'OL.CV.pdf',
+  CV_LINK = 'https://arcbjorn.com/OL.CV.pdf',
+
+  PORTFOLIO = 'https://arcbjorn.com',
+  COPY_MAIL = 'oleg.luganskiy@gmail.com',
+
+  BLOG = 'https://blog.arcbjorn.com/',
+  LEETCODE = 'https://leetcode.com/arcbjorn',
+  MY_TRAVEL_MAP = 'https://www.mytravelmap.xyz/u/gg104931485347737929283',
+
+  TWITTER = 'https://twitter.com/arcbjorn',
+  REDDIT = 'https://www.reddit.com/u/Arcbjorn',
+  INSTAGRAM = 'https://www.instagram.com/arcbjorn',
+  SPOTIFY = 'https://open.spotify.com/user/hrpn170u8d82ch90nw6y1floe',
+  GOOD_READS = 'https://www.goodreads.com/arcbjorn',
+}
