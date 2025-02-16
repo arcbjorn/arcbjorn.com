351d838314b264dfd242c300103614e394117516
arcbjorn <arky.happ@gmail.com>
2025-02-16 19:12:33 -0300

    build: add fonts & icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-
+import '@fontsource/ibm-plex-mono/400.css';
+import '@fontsource/ibm-plex-mono/500.css';
+import '@fontsource/ibm-plex-mono/600.css';
+import '@fontsource/ibm-plex-mono/700.css';
+import '@material-design-icons/font';
+import './config/iconsFA';
 import './index.css';
 import App from './App';
 
@@ -8,7 +13,7 @@ const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?'
   );
 }
 
