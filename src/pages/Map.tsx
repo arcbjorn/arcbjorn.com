78fbfda4aafc3cf2f7c14c9686b7978d4ff640e0
arcbjorn <arky.happ@gmail.com>
2025-02-23 06:43:31 -0300

    feat(components): add Map loader

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,25 +1,41 @@
-import { Component } from 'solid-js';
+import { Component, createEffect, createSignal } from 'solid-js';
 import Layout from '@layouts/Layout';
 import Map from '@components/Map';
 import SEO from '@components/SEO';
+import Loader from '@components/Loader';
 import TranslationMatrixEffect from '@components/TranslationMatrixEffect';
 
 import { EDocumentDescription } from '@/types/types';
 import { Ei18nToken } from '@i18n/types';
 import { useI18n } from '@i18n/useI18n';
 
+import animations from '@styles/animations.module.css';
+
 const MapPage: Component = () => {
+  const [isLoading, setLoading] = createSignal(true);
   const { language } = useI18n();
+
+  createEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 600);
+
+    return () => clearTimeout(timer);
+  });
 
   return (
     <Layout>
       <SEO description={EDocumentDescription.MAP} slug={`/${language()}/map`} />
-      <div class="relative flex h-full flex-col p-4">
-        <h1 class="mb-4 text-center text-2xl">
-          <TranslationMatrixEffect token={Ei18nToken.MY_TRAVEL_MAP_TITLE} lowerCase={true} />
-        </h1>
-        <Map />
-      </div>
+      {isLoading() ? (
+        <Loader />
+      ) : (
+        <div class={`relative flex h-full flex-col p-4 ${animations.fadeIn}`}>
+          <h1 class="mb-4 text-center text-2xl">
+            <TranslationMatrixEffect token={Ei18nToken.MY_TRAVEL_MAP_TITLE} lowerCase={true} />
+          </h1>
+          <Map />
+        </div>
+      )}
     </Layout>
   );
 };
