31351f96effb5c137d2f85e63d8af88266df4904
arcbjorn <arky.happ@gmail.com>
2025-09-30 14:43:01 -0300

    ref(links): AccessLinks padding

--- src/pages/AccessLinks.tsx
+++ src/pages/AccessLinks.tsx
@@ -14,7 +14,7 @@ const AccessLinksPage: Component = () => {
     <Layout>
       <SEO description={EDocumentDescription.ACCESS_LINKS} slug={`/${language()}/access_links`} />
       <div class="flex h-full flex-col items-center px-6 pt-10 sm:pt-16">
-        <h3 class="text-xl font-bold sm:text-2xl">Click to copy link:</h3>
+        <h3 class="text-xl font-bold sm:text-2xl pb-8">Click to copy link:</h3>
         <div class={`${styles.quickLinks} flex-col`}>
           <For each={quickAccessLinks}>
             {link => <QuickLink link={link} copyToClipboard={true} />}
