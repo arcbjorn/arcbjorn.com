dacf9d560555468251467981d20ce6534bc5de9e
arcbjorn <arky.happ@gmail.com>
2025-10-17 00:07:37 -0300

    ref(platforms): compact 3-icon horizontal layout

--- src/components/extraLinks/ExtraLinks.tsx
+++ src/components/extraLinks/ExtraLinks.tsx
@@ -1,6 +1,5 @@
-import { Component, For } from 'solid-js';
+import { Component } from 'solid-js';
 import { Ei18nToken } from '@i18n/types';
-import PlatformLink from '@/components/extraLinks/PlatformLink';
 import SocialLink from '@/components/extraLinks/SocialLink';
 import TranslationMatrixEffect from '@/components/TranslationMatrixEffect';
 import { platformLinks, socialLinks } from '@data/linksData';
@@ -8,22 +7,29 @@ import extraStyles from '@styles/extra.module.css';
 import styles from '@styles/extraLinks.module.css';
 
 const ExtraLinks: Component = () => {
+  // Get LeetCode from platformLinks
+  const leetcodeLink = platformLinks.find(link => link.title === 'LeetCode');
+
   return (
-    <fieldset class={extraStyles.extraInnerSection}>
-      <legend class={extraStyles.extraSectionTitle}>
-        <TranslationMatrixEffect token={Ei18nToken.PLATFORMS_TITLE} />
-      </legend>
+    <div class={styles.platformsWrapper}>
+      <fieldset class={`${extraStyles.extraInnerSection} ${styles.compactSection}`}>
+        <legend class={extraStyles.extraSectionTitle}>
+          <TranslationMatrixEffect token={Ei18nToken.PLATFORMS_TITLE} />
+        </legend>
 
-      <div class={styles.linksContainer}>
-        <div class={styles.platformLinks}>
-          <For each={platformLinks}>{link => <PlatformLink link={link} />}</For>
+        <div class={styles.linksContainer}>
+          {leetcodeLink && (
+            <>
+              <SocialLink link={leetcodeLink} />
+              <div class={styles.divider} />
+            </>
+          )}
+          <SocialLink link={socialLinks[0]} />
+          <div class={styles.divider} />
+          <SocialLink link={socialLinks[1]} />
         </div>
-
-        <div class={styles.socialLinks}>
-          <For each={socialLinks}>{link => <SocialLink link={link} />}</For>
-        </div>
-      </div>
-    </fieldset>
+      </fieldset>
+    </div>
   );
 };
 
