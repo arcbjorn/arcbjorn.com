e29cce14b3f0fff1afaded51ee8a90d3a68b4a0f
arcbjorn <arky.happ@gmail.com>
2025-10-17 00:37:35 -0300

    ref(extra): remove loader animation

--- src/pages/Extra.tsx
+++ src/pages/Extra.tsx
@@ -1,8 +1,7 @@
-import { Component, createEffect, createSignal } from 'solid-js';
+import { Component } from 'solid-js';
 import Layout from '@layouts/Layout';
 import Books from '@/components/Books';
 import Utils from '@/components/Utils';
-import Loader from '@components/Loader';
 import ExtraLinks from '@components/extraLinks/ExtraLinks';
 import Languages from '@components/languages/Languages';
 import SEO from '@components/SEO';
@@ -14,38 +13,25 @@ import styles from '@styles/extra.module.css';
 import animations from '@styles/animations.module.css';
 
 const ExtraPage: Component = () => {
-  const [isLoading, setLoading] = createSignal(true);
   const { language } = useI18n();
-
-  createEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 600);
-
-    return () => clearTimeout(timer);
-  });
 
   return (
     <Layout>
       <SEO description={EDocumentDescription.EXTRA} slug={`/${language()}/extra`} />
 
-      {isLoading() ? (
-        <Loader />
-      ) : (
-        <div class={`${styles.extraInfo} ${animations.fadeIn}`}>
-          {/* Left Column */}
-          <div class={styles.extraSection}>
-            <Utils />
-            <ExtraLinks />
-          </div>
-
-          {/* Right Column */}
-          <div class={styles.extraSection}>
-            <Books />
-            <Languages />
-          </div>
+      <div class={`${styles.extraInfo} ${animations.fadeIn}`}>
+        {/* Left Column */}
+        <div class={styles.extraSection}>
+          <Utils />
+          <ExtraLinks />
         </div>
-      )}
+
+        {/* Right Column */}
+        <div class={styles.extraSection}>
+          <Books />
+          <Languages />
+        </div>
+      </div>
     </Layout>
   );
 };
